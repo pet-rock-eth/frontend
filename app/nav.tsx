03b9e49c8e9588073ce08caec78c637b02d3d534
gnehs <jayuiop@gmail.com>
2023-05-19 15:23:51 +0800

    added connect wallet

--- app/nav.tsx
+++ app/nav.tsx
@@ -2,6 +2,9 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import { motion } from "framer-motion"
+
+import { useWeb3Modal } from "@web3modal/react";
+import { useAccount } from 'wagmi'
 function NavItem({
   href, children
 }: {
@@ -13,13 +16,13 @@ function NavItem({
   return (
     <Link
       href={href}
-      className={`relative px-3 py-2 text-xl ${current ? 'text-white font-bold' : 'text-gray-500 hover:text-gray-900'}`}
+      className={`relative px-3 py-2 text-xl ${current ? 'text-white font-bold' : 'text-white text-opacity-70 hover:text-opacity-90'}`}
     >
       {
         current &&
         <motion.div
           layoutId="underline"
-          className="absolute bottom-0 left-0 w-full h-full bg-gray-900 rounded-xl"
+          className="absolute bottom-0 left-0 w-full h-full bg-white bg-opacity-10 rounded-xl"
         />
       }
       <span className='relative'>
@@ -29,15 +32,25 @@ function NavItem({
   )
 }
 export default function Nav() {
+  const { open: openWeb3Modal, isOpen: isWeb3ModalOpen } = useWeb3Modal();
+  const { address, isConnected } = useAccount()
   return (
     <>
       <nav className="">
         <div className="container mx-auto px-6 py-2 flex justify-center items-center">
           <NavItem href='/'>寵物石頭</NavItem>
-          <NavItem href='/school'>學校</NavItem>
-          <NavItem href='/columbarium'>靈骨塔</NavItem>
           <NavItem href='/my-rock'>我ㄉ石頭</NavItem>
+          <NavItem href='/school'>石頭學校</NavItem>
+          <NavItem href='/columbarium'>石頭靈骨塔</NavItem>
           <div className='flex-1' />
+          <a className="flex items-center gap-2 px-3 py-2 rounded-xl text-xl cursor-pointer
+          hover:bg-white hover:bg-opacity-10"
+            onClick={e => openWeb3Modal()}>
+            {isConnected ? <i className='bx bx-user' ></i> : <i className='bx bx-wallet' ></i>}
+            {isConnected ? <span>{address?.slice(0, 6)}...{address?.slice(-6)}</span> :
+              isWeb3ModalOpen ? <span>正在連接...</span> :
+                <span>連接錢包</span>}
+          </a>
         </div>
       </nav>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <>
-      <h1>hi</h1>
+      <img src="/rock.png" className="w-40 mx-auto mt-16" />
+      <h1 className="text-4xl text-center mt-4 font-bold">來自區塊鏈的</h1>
+      <h1 className="text-6xl text-center mt-2 font-bold">寵物石頭</h1>
     </>
   )
 }
