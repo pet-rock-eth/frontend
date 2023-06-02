9a6d5395c156b5c1e69addf849f1ccac073b821b
gnehs <jayuiop@gmail.com>
2023-06-02 15:45:29 +0800

    added layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { goerli } from "wagmi/chains";
 import { writeContract, waitForTransaction } from "@wagmi/core";
 import { contractAddress, contractABI } from "../contract/stone";
 import { toast } from "react-toastify";
+import { motion } from "framer-motion";
 export default function Home() {
   const { open: openWeb3Modal, isOpen: isWeb3ModalOpen } = useWeb3Modal();
   const { address, isConnected } = useAccount();
@@ -37,7 +38,7 @@ export default function Home() {
       }
       //@ts-ignore
       else if (e.toString().includes("TransactionExecutionError")) {
-        showToast("請授權合約", "error");
+        showToast("請允許合約鑄造", "error");
       } else {
         showToast("鑄造失敗", "error");
       }
@@ -53,26 +54,40 @@ export default function Home() {
       <div className="flex justify-center mt-8">
         {isConnected ? (
           minting ? (
-            <a className="flex items-center gap-2 px-8 py-6 rounded-xl text-2xl cursor-pointer bg-blue-500 bg-opacity-10  shadow-lg ring-1 ring-white ring-opacity-10">
+            <motion.a
+              className="flex items-center gap-2 px-8 py-6 rounded-xl text-2xl cursor-pointer bg-blue-500 bg-opacity-10  shadow-lg ring-1 ring-white ring-opacity-10"
+              layout
+              layoutId="main-btn"
+            >
               <i className="bx bx-loader-alt animate-spin"></i> 鑄造中...
-            </a>
+            </motion.a>
           ) : (
-            <a
-              className="flex items-center gap-2 px-8 py-6 rounded-xl text-2xl cursor-pointer shadow-lg bg-blue-500 bg-opacity-20 hover:bg-opacity-40"
+            <motion.a
+              className="flex items-center gap-2 px-8 py-6 rounded-xl text-2xl cursor-pointer shadow-lg bg-blue-500 bg-opacity-20 hover:bg-opacity-40 transition-colors"
               onClick={(e) => mintStone()}
+              layout
+              layoutId="main-btn"
+              whileHover={{
+                scale: 1.1,
+              }}
+              whileTap={{
+                scale: 0.9,
+              }}
             >
               鑄造石頭
-            </a>
+            </motion.a>
           )
         ) : (
-          <a
+          <motion.a
             className="flex items-center gap-2 px-8 py-3 rounded-xl text-2xl cursor-pointer shadow-lg
             bg-blue-500 bg-opacity-20 hover:bg-opacity-40"
             onClick={(e) => openWeb3Modal()}
+            layout
+            layoutId="main-btn"
           >
             <i className="bx bx-wallet"></i>
             {isWeb3ModalOpen ? <span>正在連接...</span> : <span>連接錢包</span>}
-          </a>
+          </motion.a>
         )}
       </div>
     </div>
