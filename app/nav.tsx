3c4dfe4ff0df38efcc7aac6afc2c2335752b1f87
gnehs <jayuiop@gmail.com>
2023-06-02 16:02:13 +0800

    updated mobile nav

--- app/nav.tsx
+++ app/nav.tsx
@@ -7,16 +7,18 @@ import { useAccount } from "wagmi";
 function NavItem({
   href,
   children,
+  name,
 }: {
   href: string;
   children: React.ReactNode;
+  name?: string;
 }) {
   const pathname = usePathname();
   const current = pathname === href;
   return (
     <Link
       href={href}
-      className={`relative px-3 py-2 text-xl ${
+      className={`relative p-1 lg:p-2 text-sm lg:text-xl ${
         current
           ? "text-white font-bold"
           : "text-white text-opacity-70 hover:text-opacity-90"
@@ -28,9 +30,11 @@ function NavItem({
           className="absolute bottom-0 left-0 w-full h-full bg-white bg-opacity-10 rounded-xl"
         />
       )}
-      <span className="relative flex justify-center items-center gap-2">
+
+      <motion.span className="relative flex justify-center items-center gap-2">
         {children}
-      </span>
+        <motion.span>{name}</motion.span>
+      </motion.span>
     </Link>
   );
 }
@@ -42,22 +46,22 @@ export default function Nav() {
     <>
       <nav className="">
         <div className="container mx-auto px-6 py-2 flex justify-center items-center">
-          <NavItem href="/">
-            <i className="bx bx-home-alt"></i>寵物石頭
+          <NavItem href="/" name="寵物石頭">
+            <i className="bx bx-home-alt"></i>
           </NavItem>
-          <NavItem href="/my-rock/">
-            <i className="bx bx-user"></i> 我ㄉ石頭
+          <NavItem href="/my-rock/" name="我ㄉ">
+            <i className="bx bx-user"></i>
           </NavItem>
-          <NavItem href="/school/">
-            <i className="bx bxs-school"></i> 石頭學校
+          <NavItem href="/school/" name="學校">
+            <i className="bx bxs-school"></i>
           </NavItem>
-          <NavItem href="/columbarium/">
-            <i className="bx bx-ghost"></i> 石頭靈骨塔
+          <NavItem href="/columbarium/" name="靈骨塔">
+            <i className="bx bx-ghost"></i>
           </NavItem>
           <div className="flex-1" />
           <a
-            className="flex items-center gap-2 px-3 py-2 rounded-xl text-xl cursor-pointer
-          hover:bg-white hover:bg-opacity-10"
+            className="items-center gap-2 p-1 lg:p-2 rounded-xl text-sm lg:text-xl cursor-pointer
+          hover:bg-white hover:bg-opacity-10 flex"
             onClick={(e) => openWeb3Modal()}
           >
             {isConnected ? (
@@ -66,9 +70,13 @@ export default function Nav() {
               <i className="bx bx-wallet"></i>
             )}
             {isConnected ? (
-              <span>
-                {address?.slice(0, 6)}...{address?.slice(-6)}
-              </span>
+              <>
+                <span className="lg:hidden">錢包</span>
+                <span className="hidden lg:inline">
+                  {" "}
+                  {address?.slice(0, 6)}...{address?.slice(-6)}
+                </span>
+              </>
             ) : isWeb3ModalOpen ? (
               <span>正在連接...</span>
             ) : (
