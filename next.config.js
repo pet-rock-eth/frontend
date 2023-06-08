2574533f295910a5f1860eb29fdfdbdaa2ba0235
gnehs <jayuiop@gmail.com>
2023-06-09 01:39:20 +0800

    fixed dependency issue

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   distDir: 'dist',
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 }
 
 module.exports = nextConfig
