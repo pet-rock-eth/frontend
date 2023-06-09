044a411f0795033e0b041f00e45a00817e1404bc
gnehs <jayuiop@gmail.com>
2023-06-09 14:16:59 +0800

    added rock detail

--- app/my-rock/page.tsx
+++ app/my-rock/page.tsx
@@ -2,12 +2,110 @@
 import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
 import { goerli } from "wagmi/chains";
-import { readContract, writeContract } from "@wagmi/core";
+import { readContract, writeContract, waitForTransaction } from "@wagmi/core";
 import { contractAddress, contractABI } from "../../contract/stone";
 import { motion } from "framer-motion";
+
+import { toast } from "react-toastify";
 import CheckConnected from "../../components/checkConnected";
+function showToast(
+  msg: string,
+  type: "success" | "error" | "info" | "warning" = "success"
+) {
+  toast[type](msg);
+}
+function InfoField({ label, value }: any) {
+  return (
+    <div className="flex gap-1 justify-between items-center">
+      <div className="text-white font-bold">{label}</div>
+      <div className="text-white text-opacity-80">{value}</div>
+    </div>
+  );
+}
+function FeedBtn({
+  onClick,
+  icon,
+  text,
+}: {
+  onClick: any;
+  icon: string;
+  text: string;
+}) {
+  return (
+    <button
+      className="bg-white backdrop-blur-md bg-opacity-10 hover:bg-opacity-20 active:bg-opacity-30 rounded-[8px] p-2 shadow flex flex-col gap-2 items-center w-full group transition-all"
+      onClick={onClick}
+    >
+      <i
+        className={`bx ${icon} text-3xl group-hover:scale-125 transition-all`}
+      ></i>{" "}
+      {text}
+    </button>
+  );
+}
 function Rock({ rock }: any) {
   const [detail, setDetail] = useState(false);
+  const [detailInfo, setDetailInfo] = useState<any>(null);
+  const { address } = useAccount();
+  async function getDetail() {
+    let rockInfo: any = {};
+    const rock_struct: any = await readContract({
+      address: contractAddress,
+      abi: contractABI,
+      functionName: "get_struct",
+      args: [rock.id],
+      chainId: goerli.id,
+    });
+    rockInfo.adopt_time = new Date(
+      Number(rock_struct.adopt_date_timestamp) * 1000
+    );
+    rockInfo.adopter_address = rock_struct.adopter_address;
+    rockInfo.feed = Number(rock_struct.feed);
+    rockInfo.healthPointPercentage_18digits = Number(
+      rock_struct.healthPointPercentage_18digits
+    );
+    rockInfo.live_status = rock_struct.live_status;
+    rockInfo.lock_status = rock_struct.lock_status;
+    setDetailInfo(rockInfo);
+  }
+  useEffect(() => {
+    if (detail) {
+      getDetail();
+    }
+  }, [detail]);
+  async function feed() {
+    try {
+      showToast(`請同意合約互動來餵食「${rock.name}」`, "info");
+      const days = Math.floor(Math.random() * 5) + 1; // 1~5
+      const { hash } = await writeContract({
+        address: contractAddress,
+        abi: contractABI,
+        functionName: "feed",
+        args: [rock.id, days, address],
+        chainId: goerli.id,
+      });
+      setDetailInfo(null);
+      await waitForTransaction({ hash });
+      const yummy = ["好吃", "美味", "快樂", "開心"];
+      const randomYummy = yummy[Math.floor(Math.random() * yummy.length)];
+      showToast(`「${rock.name}」覺得${randomYummy}！`);
+      getDetail();
+    } catch (e) {
+      console.log(e);
+      //@ts-ignore
+      if (e.toString().includes("ChainMismatchError")) {
+        showToast("請切換到 Goerli 測試網", "error");
+      }
+      //@ts-ignore
+      else if (e.toString().includes("TransactionExecutionError")) {
+        showToast("請確認合約互動", "error");
+      } else {
+        showToast("餵食失敗", "error");
+      }
+    } finally {
+      getDetail();
+    }
+  }
   return detail ? (
     <>
       <div
@@ -47,10 +145,24 @@ function Rock({ rock }: any) {
             layout
             layoutId={`rock-img-${rock.id}`}
           />
-          <button className="bg-white backdrop-blur-md bg-opacity-10 hover:bg-opacity-20 active:bg-opacity-30 rounded-[8px] p-2 shadow">
-            <i className="bx bx-baguette"></i> 餵食
-          </button>
           <div className="flex flex-col gap-2">
+            <motion.div
+              className="flex gap-2 w-full"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <FeedBtn icon="bx-cake" text="餵蛋糕" onClick={() => feed()} />
+              <FeedBtn
+                icon="bx-popsicle"
+                text="餵冰棒"
+                onClick={() => feed()}
+              />
+              <FeedBtn
+                icon="bx-drink"
+                text="餵雞尾酒"
+                onClick={() => showToast(`${rock.name} 醉了`, `info`)}
+              />
+            </motion.div>
             <div>
               <motion.span
                 className="text-white text-2xl font-bold"
@@ -60,7 +172,39 @@ function Rock({ rock }: any) {
                 {rock.name}
               </motion.span>
             </div>
-            <div>{rock.description}</div>
+            <motion.div
+              className="flex flex-col gap-2"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <div>{rock.description}</div>
+              {detailInfo ? (
+                <motion.div>
+                  <InfoField label="feed" value={detailInfo.feed} />
+                  <InfoField
+                    label="healthPoint"
+                    value={detailInfo.healthPointPercentage_18digits}
+                  />
+                  <InfoField label="石頭編號 #" value={rock.id} />
+                  <InfoField
+                    label="領養時間"
+                    value={detailInfo.adopt_time.toLocaleString()}
+                  />
+                  <InfoField
+                    label="鎖定狀態"
+                    value={detailInfo.lock_status ? `是` : `否`}
+                  />
+                  <InfoField
+                    label="存活狀態"
+                    value={detailInfo.live_status ? `還沒掛` : `掰了`}
+                  />
+                </motion.div>
+              ) : (
+                <motion.div className="flex justify-center items-center h-[144px]">
+                  <i className="bx bx-loader-alt animate-spin text-4xl"></i>
+                </motion.div>
+              )}
+            </motion.div>
           </div>
         </motion.div>
       </div>
@@ -105,19 +249,21 @@ export default function MyRock() {
         chainId: goerli.id,
       });
       //@ts-ignore
-      let ids = data.map((x) => parseInt(x)).filter((x) => x > 0);
+      let ids = data.map((x) => Number(x)).filter((x) => x > 0);
       await Promise.all(
         //@ts-ignore
         ids.map(async (id) => {
+          let rockInfo: any = {};
           if (localStorage.getItem(`rock-${id}`)) {
-            return JSON.parse(localStorage.getItem(`rock-${id}`) || "{}");
+            rockInfo = JSON.parse(localStorage.getItem(`rock-${id}`) || "{}");
+          } else {
+            const res = await fetch(
+              `https://ipfs.io/ipfs/bafybeibkrtttj2mtjmuwu26l7dlbmvt5k5qgah7qxmhobv3ps5j232tzdy/stone${id}.json`
+            );
+            rockInfo = await res.json();
+            localStorage.setItem(`rock-${id}`, JSON.stringify(data));
           }
-          const res = await fetch(
-            `https://ipfs.io/ipfs/bafybeibkrtttj2mtjmuwu26l7dlbmvt5k5qgah7qxmhobv3ps5j232tzdy/stone${id}.json`
-          );
-          const data = await res.json();
-          localStorage.setItem(`rock-${id}`, JSON.stringify(data));
-          return data;
+          return rockInfo;
         })
       ).then((data) => setRocks(data));
       setLoading(false);
@@ -132,6 +278,8 @@ export default function MyRock() {
           <div className="flex justify-center items-center h-[512px]">
             <i className="bx bx-loader-alt animate-spin text-4xl"></i>
           </div>
+        ) : rocks.length === 0 ? (
+          <div className="mt-2 text-center">你還沒有石頭喔</div>
         ) : (
           <div className="container">
             <div className="mt-4 grid gap-2 grid-cols-[repeat(auto-fill,minmax(128px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(256px,1fr))]">
