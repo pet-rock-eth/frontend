662314341960f1f5b17f9a51d85e10f763115cb0
gnehs <jayuiop@gmail.com>
2023-06-09 15:23:09 +0800

    update index

--- app/page.tsx
+++ app/page.tsx
@@ -93,6 +93,33 @@ export default function Home() {
           </motion.a>
         )}
       </div>
+      <div className="container mx-auto mt-8">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div>
+            <i className="bx bx-star text-6xl"></i>
+            <h2 className="text-2xl font-bold mt-1">你的第一個區塊鏈石頭</h2>
+            <div className="mt-1">
+              來養你的第一顆區塊鏈石頭吧！石頭是一種
+              NFT，你可以在區塊鏈上查看石頭的所有權，並且在市場上交易。
+            </div>
+          </div>
+          <div>
+            <i className="bx bx-dish text-6xl"></i>
+            <h2 className="text-2xl font-bold mt-1">餵食石頭</h2>
+            <div className="mt-1">
+              石頭必須定期餵食才能維持生命，你可以在石頭的詳細資料頁面看到石頭的活力值，活力值會隨著時間流逝而降低，當活力值過低時，石頭可能會死亡。
+            </div>
+          </div>
+          <div>
+            <i className="bx bx-cuboid text-6xl"></i>
+
+            <h2 className="text-2xl font-bold mt-1">獨一無二</h2>
+            <div className="mt-1">
+              每個石頭都是獨一無二的，他們有著獨特的名字和生世背景，你可以在鑄造後看到相關內容。
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
